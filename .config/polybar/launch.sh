435be810acabba0e73ae9b84328ffd84e99c7c06
justanoobcoder <syaorancode@gmail.com>
2021-10-18 09:41:06 +0700

    launch.sh: run volume script to check headphone input

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,12 +4,17 @@ error() { echo -e "$1" && exit; }
 
 [ $EUID -eq 0 ] && error "Do not run this script as root."
 
-DIR="$HOME/.config/polybar"
+DIR="${HOME}/.config/polybar"
 
 killall -q polybar
+killall -q volume
 
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+while pgrep -u ${UID} -x polybar && pgrep -u $UID -x volume > /dev/null;
+do
+    sleep 1
+done
 
-polybar -r main -c "$DIR"/config.ini &
+${DIR}/scripts/volume --headphone &
+polybar -r main -c ${DIR}/config.ini &
 
 exit 0
